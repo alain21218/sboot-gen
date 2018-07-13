f02b137090e7075ae12461852126392b41bc6156
Leix <alain21218@gmail.com>
2018-07-14 01:26:34 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+require('./lib/text.js');
+
+const program = require('commander');
+const { generateAll, generateEntity, generateRepo, generateRest } = require('./lib/generate');
+
+program
+    .command('repo <name>')
+    .alias('r')
+    .description('Generate a repository')
+    .action(generateRepo);
+
+program
+    .command('entity <name>')
+    .alias('e')
+    .description('Generate an entity')
+    .action(generateEntity);
+
+program
+    .command('rest <name>')
+    .alias('rc')
+    .description('Generate a rest controller')
+    .action(generateRest);
+
+program
+    .command('all <name>')
+    .alias('a')
+    .description('Generate entity, rest controller and repository')
+    .action(generateAll);
+
+
+program.parse(process.argv);
